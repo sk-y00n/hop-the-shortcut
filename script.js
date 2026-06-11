bd073119b0cdc346f7328c12c4dcd5cbf94669fd
sk-y00n <161535874+sk-y00n@users.noreply.github.com>
2026-06-12 04:12:05 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const revealElements = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.15,
+  }
+);
+
+revealElements.forEach((element) => {
+  observer.observe(element);
+});
